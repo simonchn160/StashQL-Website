967f9abc10185f980aab9e00d98e0fd092cf4b58
simonchn160 <simonchn160@gmail.com>
2022-04-13 11:58:06 -0400

    clean up Demo fil

--- client/containers/Demo.jsx
+++ client/containers/Demo.jsx
@@ -53,56 +53,6 @@ const Demo = (props) => {
     })
   }, [queries, speeds]);
 
-  // useEffect(() => {
-  //   window.addEventListener('beforeunload', demoAuthor)
-  //   return () => {
-  //     window.removeEventListener('beforeunload', demoAuthor)
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   window.onunload = async function() {
-  //     var result = demoAuthor();
-  //     return result;
-  //   }
-  // }, []);
-
-  // const demoAuthor = async () => {
-  //   // e.preventDefault(e);
-  //   clearCache();
-  //   let method = 'DELETE';
-  //   await fetch('/api/demoAuthor', {
-  //     method,
-  //     headers: { 'Content-Type': 'application/json' }
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data))
-  //     .catch((err) => console.log('Errpr in deleting: ', err));
-  //   return demoAuthor;
-  // }
-
-  // useEffect(() => {
-  //   window.onunload = window.onbeforeunload = async function() {
-  //     var result = demoAuthor();
-  //     return result;
-  //   }
-  // }, []);
-
-  // const demoAuthor = async () => {
-  //   // e.preventDefault(e);
-  //   clearCache();
-  //   let method = 'DELETE';
-  //   await fetch('/api/demoAuthor', {
-  //     method,
-  //     headers: { 'Content-Type': 'application/json' }
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data))
-  //     .catch((err) => console.log('Errpr in deleting: ', err));
-  //   return demoAuthor;
-  // }
-
-
   useEffect(() => {
     window.onbeforeunload = function() {
       // e.preventDefault();
@@ -121,23 +71,6 @@ const Demo = (props) => {
       .catch((err) => console.log('Errpr in deleting: ', err));
     return demoAuthor;
   }
-
-  // const submitQuery = async () => {
-  //   let method = 'POST';
-  //   await fetch('/api/graphql', {
-  //     method,
-  //     body: JSON.stringify({query}),
-  //     headers: { 'Content-Type': 'application/json' },
-  //   })
-  //     .then((res) => res.json())
-  //     .then(({ data, runTime }) => {
-  //       setReturnedData(JSON.stringify(data, null, 2));
-  //       setRunTime(oldRunTimes => [...oldRunTimes, runTime]);
-  //       addQueries(oldQueries => [...oldQueries, `Query ${oldQueries.length + 1}`]);
-  //       addSpeeds(oldSpeeds => [...oldSpeeds, runTime]);
-  //     })
-  //     .catch((err) => console.log('Error: ', err));
-  // };
 
   const submitQuery = async () => {
     let method = 'POST';
@@ -180,74 +113,6 @@ const Demo = (props) => {
   }
 
   return (
-
-    // <div>
-    //   <Navbar demoAuthor={demoAuthor}/>
-
-    //   <div id='demoContainer'>
-      
-    //     <div id='leftSide'>
-
-    //       <div id='leftBox'>
-    //         <div id='leftBoxOptions'>
-    //           <h3 id='query1' onClick={() => {setQuery(query1); setReturnedData('')}}>Query</h3>
-    //           <h3 id='mutation1' onClick={() => {setQuery(mutation1); setReturnedData('')}}>Mutation</h3>
-    //         </div>
-    //         <textarea value={query} readOnly={true} id='selectedQuery'>
-    //         </textarea>
-    //         <div id='btnContainer'>
-    //           {query === query1 ? <button id='submitBtn' onClick={() => {submitQuery()}}>Submit Query</button> : <button id='submitBtn' onClick={() => {submitMutation()}}>Submit Mutation</button>}
-    //           <button id='clearBtn' onClick={() => {clearCache()}}>Clear Cache</button>
-    //         </div>
-
-    //       </div>
-
-    //       <div id='rightBox'>
-    //         <div id='rightBoxOptions'>
-    //           <h3>Returned Data</h3>
-    //         </div>
-    //         <textarea value={returnedData} readOnly={true} id='returnedData'>
-    //         </textarea>
-    //       </div>
-
-    //     </div>
-
-    //     <div id='rightSide'>
-    //       <div id='rightBoxOptions'>
-    //         <h3 id='querySpeed'>Query Speeds</h3>
-    //       </div>
-    //       <div id='chart'>
-    //         <Line data={userData} options={{
-    //           scales: {
-    //             yAxes: {
-    //               min: 0,
-    //               max: 450,
-    //               ticks: {
-    //                 color: 'black',
-    //                 font: {
-    //                   size: 17
-    //                 }
-    //               }
-    //             },
-    //             xAxes: {
-    //               ticks: {
-    //                 color: 'black'
-    //               }
-    //             },
-    //           },
-    //         }}/>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //    <div id='demoText'>
-    //      <h1>See first-hand what StashQL can do for your GraphQL applications</h1>
-    //      <p>Upon the first time submitting the query above, StashQL will cache the returned data. Running the query again will retrieve the data from the cache, resulting in faster queries and improved performance.</p>
-    //      <h2>What about mutations?</h2>
-    //      <p>In the mutation example above, the refillCache argument is passed in with a value of "authors". Upon seeing this, StashQL will update any data within the cache that queried for authors</p>
-    //    </div>
-
-    // </div>
 
     <div>
       <Navbar demoAuthor={demoAuthor}/>
@@ -309,11 +174,6 @@ const Demo = (props) => {
                     padding: 5,
                     color: '#fff'
                   },
-                  // grid: {
-                  //   color: (context) => {
-                  //     return '#fff';
-                  //   }
-                  // },
                   min: 0,
                   max: 500,
                   ticks: {
@@ -333,11 +193,6 @@ const Demo = (props) => {
                     padding: 5,
                     color: '#fff'
                   },
-                  // grid: {
-                  //   color: (context) => {
-                  //     return '#fff';
-                  //   },
-                  // },
                   ticks: {
                     color: '#868792'
                   },
