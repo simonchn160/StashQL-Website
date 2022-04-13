b80964b2ed770823a94d2f7c3edec38a34e4de78
simonchn160 <simonchn160@gmail.com>
2022-04-13 01:03:00 -0400

    change to GET method

--- client/containers/Demo.jsx
+++ client/containers/Demo.jsx
@@ -157,9 +157,9 @@ const Demo = (props) => {
   };
 
   const submitMutation = async () => {
-    let method = 'PUT';
+    // let method = 'PUT';
     await fetch('/api/graphql', {
-      method,
+      // method,
       body: JSON.stringify({query}),
       headers: { 'Content-Type': 'application/json' },
     })
