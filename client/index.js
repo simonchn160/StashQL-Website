c076bff08fc2e4241cd9c030cb0e89eb349833e0
simonchn160 <simonchn160@gmail.com>
2022-04-13 00:48:02 -0400

    had re-routing

--- client/index.js
+++ client/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import App from "./App.jsx";
+import { createBrowserHistory } from 'history';
 
 import styles from "./css/styles.scss";
+
+
+const replaceHashPath = () => {
+  const history = createBrowserHistory()
+  const hash = history.location.hash
+  if (hash) {
+    const path = hash.replace(/^#/, '')
+    if (path) {
+      history.replace(path)
+    }
+  }
+}
+replaceHashPath()
+
+
 // import styles from "../public/css/styles.scss";
 
 render(<App />, document.getElementById("root"));
